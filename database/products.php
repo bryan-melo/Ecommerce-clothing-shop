31c5cdd005a7d69efec708aa91945b6e36e153cc
shivanshchhabra <https://github.com/shan18u/Android.git>
2023-07-21 13:23:51 -0700

    Fetch products data from Database

---
+++ database/products.php
@@ -0,0 +1,49 @@
+<?php
+
+// This class is used to fetch product data from a database.
+class Product {
+    public $db = null;
+
+    
+    // Constructor that takes a DBController object as input.
+    // If the provided DBController object doesn't have a valid connection,
+    // Then the constructor returns null.
+
+    public function __construct(DBController $db) {
+        if (!isset($db->con)) return null;
+        $this->db = $db;
+    }
+
+    // Fetches all product data from a specified table using the getData method.
+    public function getData($table = 'product') {
+        $result = $this->db->con->query("SELECT * FROM {$table}");
+
+        $resultArray = array();
+
+        // Fetches product data one by one and adds them to the result array.
+        while ($item = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
+            $resultArray[] = $item;
+        }
+
+        return $resultArray;
+    }
+
+    // Gets product data based on the provided item_id and table.
+    public function getProduct($item_id = null, $table = 'product') {
+        if (isset($item_id)) {
+            $result = $this->db->con->query
+            ("SELECT * 
+            FROM {$table} 
+            WHERE item_id={$item_id}");
+
+            $resultArray = array();
+
+            // Fetches product data one by one and adds them to the result array.
+            while ($item = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
+                $resultArray[] = $item;
+            }
+
+            return $resultArray;
+        }
+    }
+}
